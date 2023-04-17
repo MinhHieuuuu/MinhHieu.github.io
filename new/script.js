15c656af193097c40b7a1ca3433559e29c9ae45e
MinhHieuuuu <ptvodoilp@gmail.com>
2023-04-18 02:10:24 +0700

    main

--- new/script.js
+++ new/script.js
@@ -12,3 +12,19 @@ function changeColor() {
 }
 
 setInterval(changeColor, 5000);
+
+// Lấy đối tượng avt
+const avt = document.querySelector('.avt');
+
+// Thêm sự kiện khi di chuột vào avt
+avt.addEventListener('mouseenter', function() {
+  // Thêm class "active" để hiệu ứng phóng to
+  this.classList.add('active');
+});
+
+// Thêm sự kiện khi di chuột ra khỏi avt
+avt.addEventListener('mouseleave', function() {
+  // Loại bỏ class "active" để hiệu ứng phóng to
+  this.classList.remove('active');
+});
+
